fc65aef7adff35f601efb0965d78403fed319be2
Khalilmouadhen <33359182+khalilmouadhen@users.noreply.github.com>
2018-03-01 09:11:50 +0100

    Update tp1exercice1.c

--- tp1exercice1.c
+++ tp1exercice1.c
@@ -4,30 +4,46 @@
 int main(void) 
 {
     int *p,*q;
-    puts("Donner un entier : ");
-    scanf("%d",&p);
+ p=malloc(sizeof(*p));
+	if (p==NULL)
+	
+	puts("erreur de memoire");
+	else 
+	{
     q=p+1;
-    printf("p= %d \n",&p);
-    printf("q= %d \n",&q);
-    printf("int= %d \n",sizeof(int));
+    printf("p= %x \n",p);
+    printf("q= %x \n",q);
+    printf("q-p= %d \n",p-q);
+    printf("taille du type= %d \n",sizeof(int));
+    }
 
     double *p1,*q1;
-    p1=malloc(sizeof(*p1));
-    puts("Donner un reel : ");
-    scanf("%lf",&p);
+     p1=malloc(sizeof(*p1));
+	if (p1==NULL)
+	
+	puts("erreur de memoire");
+	else 
+	{
     q1=p1+1;
-    printf("p1= %d \n",&p1);
-    printf("q1= %d \n",&q1);
-    printf("double= %d \n",sizeof(double));
+    printf("p1= %x \n",p1);
+    printf("q1= %x \n",q1);
+    printf("q1-p1= %d \n",p1-q1);
+    printf("taille du type= %d \n",sizeof(double));
+    }
 
     char *p2,*q2;
-    p2=malloc(sizeof(*p2));
-    puts("Donner une chaine : ");
-    scanf("%s",&p2);
+     p2=malloc(sizeof(*p2));
+	if (p2==NULL)
+	
+	puts("erreur de memoire");
+	else 
+	{
     q2=p2+1;
-    printf("p2= %d \n",&p2);
-    printf("q2= %d \n",&q2);
-    printf("int= %d \n",sizeof(char));
-
+    printf("p2= %x \n",p2);
+    printf("q2= %x \n",q2);
+    printf("q2-p2= %d \n",p2-q2);
+    printf("taille du type= %d \n",sizeof(char));
+    }
+return 0;
 
 }
